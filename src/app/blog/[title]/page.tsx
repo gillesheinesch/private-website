10c76c3397e4eceed5c928afba5277cf2163a737
Gilles Heinesch <gilles@heinesch.com>
2025-07-24 14:36:37 +0200

    Refactored Next.js configuration and updated dependencies
    - Removed `eslint.ignoreDuringBuilds` from `next.config.ts` to ensure proper validation during builds.
    - Updated `next` version from `15.0.3` to `^15.4.3` in `package.json` and `package-lock.json` for improved features and bug fixes.
    - Adjusted various package versions in `package-lock.json` to maintain compatibility and enhance performance.
    - Cleaned up import statements in `src/app/page.tsx` and `src/app/blog/[title]/page.tsx` for better readability and organization.

--- src/app/blog/[title]/page.tsx
+++ src/app/blog/[title]/page.tsx
@@ -1,9 +1,9 @@
-import matter from 'gray-matter';
-import fs from 'fs';
-import path from 'path';
-import moment from 'moment';
 import { Box, Chip, Container, Typography } from "@mui/material";
-import ReactMarkdown from 'react-markdown';
+import fs from "fs";
+import matter from "gray-matter";
+import moment from "moment";
+import path from "path";
+import ReactMarkdown from "react-markdown";
 
 const MarkdownH1 = (props: any) => <Typography variant="h4" component="h1" gutterBottom {...props} />;
 const MarkdownH2 = (props: any) => <Typography variant="h5" component="h2" gutterBottom {...props} />;
@@ -11,11 +11,6 @@ const MarkdownH3 = (props: any) => <Typography variant="h6" component="h3" gutte
 const MarkdownP = (props: any) => <Typography variant="body1" component="p" paragraph {...props} />;
 const MarkdownA = (props: any) => <Typography variant="body1" color="primary" component="a" {...props} />;
 const MarkdownLI = (props: any) => <Typography variant="body1" component="li" {...props} />;
-
-type Params = {
-    date: string;
-    title: string;
-};
 
 interface Blog {
     title: string;
@@ -28,7 +23,7 @@ interface Blog {
 }
 
 export async function generateMetadata() {
-    const blogDirectory = path.join(process.cwd(), 'blog');
+    const blogDirectory = path.join(process.cwd(), "blog");
     const filenames = fs.readdirSync(blogDirectory);
 
     let date = "";
@@ -36,18 +31,18 @@ export async function generateMetadata() {
 
     await filenames.map((filename) => {
         const filePath = path.join(blogDirectory, filename);
-        const fileContent = fs.readFileSync(filePath, 'utf8');
+        const fileContent = fs.readFileSync(filePath, "utf8");
         const { data } = matter(fileContent);
 
-        date = data.date
-        title = encodeURIComponent(data.title)
+        date = data.date;
+        title = encodeURIComponent(data.title);
     });
     const decodedTitle = decodeURIComponent(title);
 
     const blog: Blog | undefined = filenames
         .map((filename) => {
             const filePath = path.join(blogDirectory, filename);
-            const fileContent = fs.readFileSync(filePath, 'utf8');
+            const fileContent = fs.readFileSync(filePath, "utf8");
             const { data } = matter(fileContent);
 
             return {
@@ -55,20 +50,16 @@ export async function generateMetadata() {
                 description: data.description,
                 category: data.category,
                 thumbnail: data.thumbnail,
-                date: filename.split('.')[0],
+                date: filename.split(".")[0],
                 tags: data.tags,
                 content: data.content,
             };
         })
-        .find(
-            (blog) =>
-                blog.title === decodedTitle &&
-                blog.date === date
-        );
+        .find((blog) => blog.title === decodedTitle && blog.date === date);
 
     if (!blog) {
         return {
-            title: 'Blog not found',
+            title: "Blog not found",
         };
     }
 
@@ -79,7 +70,7 @@ export async function generateMetadata() {
 }
 
 export default async function Page() {
-    const blogDirectory = path.join(process.cwd(), 'blog');
+    const blogDirectory = path.join(process.cwd(), "blog");
     const filenames = fs.readdirSync(blogDirectory);
 
     let date = "";
@@ -87,18 +78,18 @@ export default async function Page() {
 
     await filenames.map((filename) => {
         const filePath = path.join(blogDirectory, filename);
-        const fileContent = fs.readFileSync(filePath, 'utf8');
+        const fileContent = fs.readFileSync(filePath, "utf8");
         const { data } = matter(fileContent);
 
-        date = data.date
-        title = encodeURIComponent(data.title)
+        date = data.date;
+        title = encodeURIComponent(data.title);
     });
     const decodedTitle = decodeURIComponent(title);
 
     const blog: Blog | undefined = filenames
         .map((filename) => {
             const filePath = path.join(blogDirectory, filename);
-            const fileContent = fs.readFileSync(filePath, 'utf8');
+            const fileContent = fs.readFileSync(filePath, "utf8");
             const { data, content } = matter(fileContent);
 
             return {
@@ -106,16 +97,12 @@ export default async function Page() {
                 description: data.description,
                 category: data.category,
                 thumbnail: data.thumbnail,
-                date: filename.split('.')[0],
+                date: filename.split(".")[0],
                 tags: data.tags,
                 content: content,
             };
         })
-        .find(
-            (blog) =>
-                blog.title === decodedTitle &&
-                blog.date === date
-        );
+        .find((blog) => blog.title === decodedTitle && blog.date === date);
 
     if (!blog) {
         return <div>Blog not found</div>;
@@ -126,11 +113,11 @@ export default async function Page() {
             <Typography variant="h3" component="h1" gutterBottom>
                 {blog.title}
             </Typography>
-            <Typography variant="body2" color="text.secondary" sx={{ textTransform: 'uppercase' }}>
+            <Typography variant="body2" color="text.secondary" sx={{ textTransform: "uppercase" }}>
                 {blog.category}
             </Typography>
             <Typography variant="body2" color="text.secondary">
-                {moment(blog.date).format('DD.MM.YYYY')}
+                {moment(blog.date).format("DD.MM.YYYY")}
             </Typography>
             <Box sx={{ mt: 2 }}>
                 {blog.tags.map((tag: string) => (
